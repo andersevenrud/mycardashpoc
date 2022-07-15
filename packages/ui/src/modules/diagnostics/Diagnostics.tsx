312bf13bf6de1bb590a541a39927c23603341223
Anders Evenrud <andersevenrud@gmail.com>
2022-07-15 23:32:53 +0200

    feat(ui): add sparkline in metrics table

--- packages/ui/src/modules/diagnostics/Diagnostics.tsx
+++ packages/ui/src/modules/diagnostics/Diagnostics.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import { subMinutes } from 'date-fns'
+import { Sparklines, SparklinesLine } from 'react-sparklines-typescript'
 import { useToasterErrorHandler, useToaster } from '~/providers/toaster'
 import { classNames } from '~/utils'
 import Module from '~/components/Module'
 import api from '~/services/obd'
 import type { PropsWithChildren } from 'react'
+import type { OBDMetric } from '~/services/obd'
 
 function Button({
   large,
@@ -89,8 +91,9 @@ export function DiagnosticsCodes() {
 
 export function DiagnosticsMetrics() {
   const wrap = useToasterErrorHandler()
+  const pause = useRef(false)
   const { addToast } = useToaster()
-  const [table, setTable] = useState<[string, number][]>([])
+  const [table, setTable] = useState<OBDMetric[]>([])
 
   const load = () =>
     wrap(async () => {
@@ -109,43 +112,69 @@ export function DiagnosticsMetrics() {
         })
       }
 
-      const tableValues = result
-        .filter(({ metric }) => {
-          return metric.name.startsWith('obd_')
-        })
-        .map(({ metric, values }) => {
-          return [
-            metric.name.replace('obd_', ''),
-            values[values.length - 1].value,
-          ]
-        })
+      const tableValues = result.filter(({ metric }) => {
+        return metric.name.startsWith('obd_')
+      })
 
-      setTable(tableValues as [string, number][])
+      setTable(tableValues)
     })
 
-  const onRefresh = () => load()
+  const initialize = () => {
+    let loaded = false
+
+    const interval = setInterval(() => {
+      if (!pause.current && loaded) {
+        load()
+      }
+    }, 2500)
+
+    load().then(() => (loaded = true))
+
+    return interval
+  }
+
+  const onTogglePause = () => (pause.current = !pause.current)
 
   useEffect(() => {
-    load()
+    const interval = initialize()
+
+    return () => {
+      clearInterval(interval)
+    }
   }, [])
 
   return (
     <div className="flex h-full shrink grow flex-col space-y-4 overflow-hidden">
       <div className="flex justify-end space-x-2">
-        <Button onClick={onRefresh}>Refresh</Button>
+        <Button onClick={onTogglePause}>
+          {pause.current ? 'Resume' : 'Pause'}
+        </Button>
         <Button to="/diagnostics">Back</Button>
       </div>
       <div className="shrink grow overflow-auto">
-        <table className="w-full">
-          <tbody>
-            {table.map(([name, value]) => (
-              <tr key={name}>
-                <td>{name}</td>
-                <td>{value}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        {table.length === 0 && (
+          <div className="pt-12 text-center font-bold">No metrics</div>
+        )}
+
+        {table.map(({ metric: { name }, values }) => (
+          <div key={name} className="space-y-2 p-2">
+            <div className="flex justify-between">
+              <div>{name.replace('obd_', '').replace(/_/g, ' ')}</div>
+              <div>{values[values.length - 1].value}</div>
+            </div>
+            <div className="h-8">
+              <Sparklines
+                data={values.map(({ value }) => value)}
+                limit={Math.min(values.length, 50)}
+                height={20}
+                width={1000}
+                style={{ width: '100%' }}
+              >
+                <SparklinesLine color="white" />
+              </Sparklines>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
