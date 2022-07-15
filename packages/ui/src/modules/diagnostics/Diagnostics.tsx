4ab8c6e86600c34bb76245a6f075e1f682017375
Anders Evenrud <andersevenrud@gmail.com>
2022-07-15 03:02:49 +0200

    feat(ui): improve diagnostics layouts

--- packages/ui/src/modules/diagnostics/Diagnostics.tsx
+++ packages/ui/src/modules/diagnostics/Diagnostics.tsx
@@ -2,28 +2,32 @@ import React, { useEffect, useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import { subMinutes } from 'date-fns'
 import { useToasterErrorHandler, useToaster } from '~/providers/toaster'
+import { classNames } from '~/utils'
 import Module from '~/components/Module'
 import api from '~/services/obd'
 import type { PropsWithChildren } from 'react'
 
 function Button({
+  large,
   to,
   onClick,
   children,
-}: PropsWithChildren<{ to?: string; onClick?: () => void }>) {
-  const classNames =
-    'flex items-center justify-center rounded-xl bg-black/50 p-8 font-bold shadow-lg hover:bg-black/80 hover:shadow-sm'
+}: PropsWithChildren<{ large?: boolean; to?: string; onClick?: () => void }>) {
+  const classes = classNames(
+    'flex items-center justify-center rounded-xl bg-black/50 font-bold shadow-lg hover:bg-black/80 hover:shadow-sm',
+    large ? 'p-8' : 'px-4 py-2'
+  )
 
   if (to) {
     return (
-      <Link className={classNames} to={to}>
+      <Link className={classes} to={to}>
         {children}
       </Link>
     )
   }
 
   return (
-    <button type="button" className={classNames} onClick={onClick}>
+    <button type="button" className={classes} onClick={onClick}>
       {children}
     </button>
   )
@@ -32,23 +36,47 @@ function Button({
 export function DiagnosticsCodes() {
   const wrap = useToasterErrorHandler()
   const { addToast } = useToaster()
+  const [table, setTable] = useState<any[]>([])
+
+  const onClearCodesClick = () =>
+    wrap(async () => {
+      const result = await api.codes.clear()
+
+      addToast({
+        type: 'info',
+        message: result ? 'Codes cleared' : 'No codes cleared',
+      })
+    })
 
   useEffect(() => {
     wrap(async () => {
       const result = await api.codes.read()
-
-      if (!result.length) {
-        addToast({
-          type: 'info',
-          message: 'No codes found',
-        })
-      }
+      setTable(result)
     })
   }, [])
 
   return (
-    <div>
-      <Button to="/diagnostics">Back</Button>
+    <div className="flex h-full shrink grow flex-col space-y-4 overflow-hidden">
+      <div className="flex justify-end space-x-2">
+        <Button onClick={onClearCodesClick}>Clear Codes</Button>
+        <Button to="/diagnostics">Back</Button>
+      </div>
+      <div className="shrink grow overflow-auto">
+        {table.length > 0 ? (
+          <table className="w-full">
+            <tbody>
+              {table.map(([name, value]) => (
+                <tr key={name}>
+                  <td>{name}</td>
+                  <td>{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div className="pt-12 text-center font-bold">No codes</div>
+        )}
+      </div>
     </div>
   )
 }
@@ -118,24 +146,14 @@ export function DiagnosticsMetrics() {
 }
 
 export function DiagnosticsHome() {
-  const wrap = useToasterErrorHandler()
-  const { addToast } = useToaster()
-
-  const onClearCodesClick = () =>
-    wrap(async () => {
-      const result = await api.codes.clear()
-
-      addToast({
-        type: 'info',
-        message: result ? 'Codes cleared' : 'No codes cleared',
-      })
-    })
-
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <Button to="/diagnostics/codes">View codes</Button>
-      <Button to="/diagnostics/metrics">View metrics</Button>
-      <Button onClick={onClearCodesClick}>Clear Codes</Button>
+    <div className="flex justify-center space-x-4">
+      <Button large to="/diagnostics/codes">
+        View codes
+      </Button>
+      <Button large to="/diagnostics/metrics">
+        View metrics
+      </Button>
     </div>
   )
 }
